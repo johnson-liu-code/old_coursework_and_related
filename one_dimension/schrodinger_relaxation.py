2228a677695991e7f3e6375599508ff964f08a1c
jliu25 <j_liu21@u.pacific.edu>
2021-01-07 13:29:49 -0800

    20210107

--- one_dimension/schrodinger_relaxation.py
+++ one_dimension/schrodinger_relaxation.py
@@ -3,34 +3,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-h = 1.E-2
+#h = 1.E-2
 a = 1.
 
-length = a/h
+#length = a/h
+length = 1
+delta_x = .001
 
-psi = np.ones(int(length))
+spread = int(length/delta_x)
 
-m = 1;
-h_bar = 1;
+psi = np.ones(spread)
+psi[0] = 0
+#psi[int(spread/2.):-2] = -1.
+psi[-1] = 0
 
-n=1.;
-E = (n**2. * np.pi**2. * h_bar**2.) / (2 * m * a**2.);
-#print(E)
-V = 0;
+m = 1
+h_bar = 1
 
-#c = -((2 * m) / h_bar^2) * (E - V);
+n = 2.
+E = (n**2. * np.pi**2. * h_bar**2.) / (2. * m * a**2.)
+print(E)
 
-c = -2*m*h**2. / h_bar**2.
+#V = 0
+
+#c = -((2 * m) / h_bar^2) * (E - V)
+
+c = -2*m*delta_x**2. / h_bar**2.
+
+def get_potential(x):
+    if x < (1./5)*length:
+        V = 0.
+    elif x >= (1./5)*length and x < (2./5)*length:
+        V = 0.
+    elif x >= (2./5)*length and x < (3./5)*length:
+        V = 0.
+    elif x >= (3./5)*length and x < (4./5)*length:
+        V = 0.
+    elif x >= (4./5)*length:
+        V = 0.
+    else:
+        V = 0.
+
+    return V
 
 #print(c*-E)
 
-for j in range(100):
-    for i in range(int(length)-2):
+time = 1000
+
+for j in range(time):
+    #psi_new = np.copy(psi)
+    for i in range(1, spread-1):
         #psi[i+1] = (V - E - (2*c)/h**2. )**-1. * (c/h**2.) * ( psi[i] + psi[i+2] )
-        psi[i+1] = (c*(E-V)+2.)**-1. * (psi[i] + psi[i+2])
+        #psi[i+1] = (c*(E-V)+2.)**-1. * (psi[i] + psi[i+2])
+        #psi_new[i+1] = (c*(E-V)+2.)**-1. * (psi[i] + psi[i+2])
+        V = get_potential(i*delta_x)
+        #psi_new[i] = (c*(E-V)+2.)**-1. * (psi[i-1] + psi[i+1])
+        psi[i] = (c*(E-V)+2.)**-1. * (psi[i-1] + psi[i+1])
         #print(new)
+    #psi = np.copy(psi_new)
 
 
-plt.plot(np.linspace(0, a, int(length)), psi)
+#print(length)
+#print(delta_x)
+#print(spread)
+
+plt.plot(np.linspace(0, delta_x, spread), psi)
+
+plt.grid()
+plt.ylim(-1.5, 1.5)
 
 plt.show()
--- relaxation.py
+++ relaxation.py
@@ -13,21 +13,21 @@ a = 1
 p = 1
 n = 1
 
-# E = (n**2 * p**2 * h_bar**2) / (2 * m * a**2)
-E = -1
+E = (n**2 * p**2 * h_bar**2) / (2 * m * a**2)
+#E = -1
 C = -(2*m)/h_bar**2 * dx**2
 
 length = 1
 intervals = int(length/dx)
-time = 100
+time = 2
 
 # mat = (C * (E - V) + 2)**-1 * np.diag(np.ones(intervals-1), 1) + np.diag(np.ones(intervals-1), -1)
 
 mat = np.diag(np.ones(intervals-1), 1) + np.diag(np.ones(intervals-1), -1)
 mat[0], mat[-1] = 0, 0
-print mat
+#print(mat)
 
-'''
+
 psi = np.ones(intervals).reshape(intervals, 1)
 # psi[int(intervals/2),:] = -2
 
@@ -36,10 +36,19 @@ psi[-1][0] = 0
 
 data = np.zeros( (intervals, time) )
 data[:,0] = psi[:,0]
+
+#print(data[:,0])
+#print(psi)
+#print(psi[:,0])
+
+
 for t in range(1, time):
     psi = np.dot(mat, psi)
     data[:,t] = psi[:,0]
+    #print(psi)
 
+
+'''
 fig, ax = plt.subplots()
 ax.set(xlim=(0, intervals), ylim=(-500, 500))
 ax.set(xlim=(0, intervals))
